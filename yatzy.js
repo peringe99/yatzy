fca9bf2388d01d67886a60f630887b34efee84b1
Ali.AlRubaye <ali_nasrat@hotmail.com>
2020-12-07 12:43:14 +0100

    add javascript & event

---
+++ yatzy.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    let btn = document.getElementById('btnSend');
+
+    let sumtd = document.getElementById('sum');
+    let bonus = document.getElementById('bonus');
+
+    let ones = document.getElementById('ones');
+    let twos = document.getElementById('twos');
+    let threes = document.getElementById('threes');
+    let fours = document.getElementById('fours');
+    let fives = document.getElementById('fives');
+    let sixes = document.getElementById('sixes');
+    btn.addEventListener('click', function(e) {
+        let sum = parseInt(ones.value) + parseInt(twos.value) + parseInt(threes.value) + parseInt(fours.value) + parseInt(fives.value) + parseInt(sixes.value);
+        sumtd.innerHTML = sum
+
+
+    })
+
+});
