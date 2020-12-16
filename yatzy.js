ebc7b3edad5bfd9693b74e012909f8315cf26d7b
Gabriel Holmkvist <gabbeholman99@gmail.com>
2020-12-16 13:45:37 +0100

    lösningförslag

--- yatzy.js
+++ yatzy.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function(e) {
     let sumtd = document.getElementById('sum');
     let bonus = document.getElementById('bonus');
 
-    let ones = document.getElementById('ones');
-    let twos = document.getElementById('twos');
-    let threes = document.getElementById('threes');
-    let fours = document.getElementById('fours');
-    let fives = document.getElementById('fives');
-    let sixes = document.getElementById('sixes');
+    let ones1 = document.getElementById('ones1');
+    let twos1 = document.getElementById('twos1');
+    let threes1 = document.getElementById('threes1');
+    let fours1 = document.getElementById('fours1');
+    let fives1 = document.getElementById('fives1');
+    let sixes1 = document.getElementById('sixes1');
     btn.addEventListener('click', function(e) {
-        let sum = parseInt(ones.value) + parseInt(twos.value) + parseInt(threes.value) + parseInt(fours.value) + parseInt(fives.value) + parseInt(sixes.value);
+        let sum = parseInt(ones1.value) + parseInt(twos1.value) + parseInt(threes1.value) + parseInt(fours1.value) + parseInt(fives1.value) + parseInt(sixes1.value);
         sumtd.innerHTML = sum
 
         if (sum >= 63) {
